210b57ce7a1ffbb79c056452e8fc008477f6b35d
ruslan <ruslan1123@gmail.com>
2019-04-29 03:26:01 +0300

    feat: Experiment skaggle_010

--- kernel_template.py
+++ kernel_template.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import Dict
 
-EXPERIMENT_NAME = 'skaggle_009'
+EXPERIMENT_NAME = 'skaggle_010'
 KERNEL_MODE = "train"  # "train" or "predict"
 
 # this is base64 encoded source code
