cad5a935d06a008b59c94fec6b5498a3a0226242
ruslan <ruslan1123@gmail.com>
2019-05-04 14:54:52 +0300

    feat: Add optional se model feat: Add conv bias param

--- src/models/simple_kaggle.py
+++ src/models/simple_kaggle.py
@@ -3,21 +3,43 @@ from torch import nn
 import torch.nn.functional as F
 
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
+class SEModule(nn.Module):
+    def __init__(self, channels, reduction):
+        super(SEModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1,
+                             padding=0)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1,
+                             padding=0)
+        self.sigmoid = nn.Sigmoid()
 
+    def forward(self, x):
+        module_input = x
+        x = self.avg_pool(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return module_input * x
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, se_module=False, conv_bias=True):
+        super().__init__()
+        self.se_module = se_module
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=conv_bias),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=conv_bias),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
         )
-
+        if self.se_module:
+            self.se = SEModule(out_channels, 16)
         self._init_weights()
 
     def _init_weights(self):
@@ -34,18 +56,25 @@ class ConvBlock(nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = F.avg_pool2d(x, 2)
+        if self.se_module:
+            x = self.se(x)
         return x
 
 
 class SimpleKaggle(nn.Module):
-    def __init__(self, num_classes, base_size=64, dropout=0.2):
+    def __init__(self, num_classes, base_size=64, dropout=0.2,
+                 se_module=False, conv_bias=True):
         super().__init__()
 
         self.conv = nn.Sequential(
-            ConvBlock(in_channels=3, out_channels=base_size),
-            ConvBlock(in_channels=base_size, out_channels=base_size*2),
-            ConvBlock(in_channels=base_size*2, out_channels=base_size*4),
-            ConvBlock(in_channels=base_size*4, out_channels=base_size*8),
+            ConvBlock(in_channels=3, out_channels=base_size,
+                      se_module=se_module, conv_bias=conv_bias),
+            ConvBlock(in_channels=base_size, out_channels=base_size*2,
+                      se_module=se_module, conv_bias=conv_bias),
+            ConvBlock(in_channels=base_size*2, out_channels=base_size*4,
+                      se_module=se_module, conv_bias=conv_bias),
+            ConvBlock(in_channels=base_size*4, out_channels=base_size*8,
+                      se_module=se_module, conv_bias=conv_bias),
         )
 
         self.fc = nn.Sequential(
