fcf18b79406f90264f67113865c772afba621a26
ruslan <ruslan1123@gmail.com>
2019-05-15 21:48:37 +0300

    feat: Experiment delta_feature_001

--- kernel_template.py
+++ kernel_template.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import Dict
 
-EXPERIMENT_NAME = 'noisy_mixup_001'
+EXPERIMENT_NAME = 'delta_feature_001'
 KERNEL_MODE = "train"  # "train" or "predict"
 
 # this is base64 encoded source code
