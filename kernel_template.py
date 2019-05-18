149515e0bab092e4a23555daaaf7fd4f49cf2a5d
ruslan <ruslan1123@gmail.com>
2019-05-18 22:16:38 +0300

    feat: Experiment simple_attention_002

--- kernel_template.py
+++ kernel_template.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import Dict
 
-EXPERIMENT_NAME = 'delta_feature_002'
+EXPERIMENT_NAME = 'simple_attention_002'
 KERNEL_MODE = "predict"  # "train" or "predict"
 
 # this is base64 encoded source code
