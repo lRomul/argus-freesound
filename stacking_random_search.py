7b01929165fa5cf55444a3872b269064d0868598
ruslan <ruslan1123@gmail.com>
2019-05-28 02:17:41 +0300

    feat: Stacking random search fcnet_stacking_rs_001

---
+++ stacking_random_search.py
@@ -0,0 +1,124 @@
+import json
+import time
+import torch
+import random
+import numpy as np
+from pprint import pprint
+
+from argus.callbacks import MonitorCheckpoint, \
+    EarlyStopping, LoggingToFile, ReduceLROnPlateau
+
+from torch.utils.data import DataLoader
+
+from src.stacking.datasets import get_out_of_folds_data, StackingDataset
+from src.stacking.transforms import get_transforms
+from src.stacking.argus_models import StackingModel
+from src import config
+
+EXPERIMENT_NAME = 'fcnet_stacking_rs_001'
+START_FROM = 0
+EXPERIMENTS = [
+    'corrections_005',
+    'corrections_006',
+    'auxiliary_001'
+]
+DATASET_SIZE = 128 * 256
+CORRECTIONS = True
+if config.kernel:
+    NUM_WORKERS = 2
+else:
+    NUM_WORKERS = 8
+SAVE_DIR = config.experiments_dir / EXPERIMENT_NAME
+
+
+def train_folds(save_dir, folds_data):
+    random_params = {
+        'base_size': int(np.random.choice([32, 64, 128, 256])),
+        'reduction_scale': int(np.random.choice([4, 8, 16, 32])),
+        'p_dropout': float(np.random.uniform(0.0, 0.5)),
+        'lr': float(np.random.uniform(0.003, 0.00003)),
+        'patience': int(np.random.randint(3, 30)),
+        'factor': float(np.random.uniform(0.5, 0.8)),
+        'batch_size': int(np.random.choice([32, 64, 128, 256])),
+    }
+    pprint(random_params)
+
+    save_dir.mkdir(parents=True, exist_ok=True)
+    with open(save_dir / 'random_params.json', 'w') as outfile:
+        json.dump(random_params, outfile)
+
+    params = {
+        'nn_module': ('FCNet', {
+            'in_channels': len(config.classes) * len(EXPERIMENTS),
+            'num_classes': len(config.classes),
+            'base_size': random_params['base_size'],
+            'reduction_scale': random_params['reduction_scale'],
+            'p_dropout': random_params['p_dropout']
+        }),
+        'loss': 'BCEWithLogitsLoss',
+        'optimizer': ('Adam', {'lr': random_params['lr']}),
+        'device': 'cuda',
+    }
+
+    for fold in config.folds:
+        val_folds = [fold]
+        train_folds = list(set(config.folds) - set(val_folds))
+        save_fold_dir = save_dir / f'fold_{fold}'
+        print(f"Val folds: {val_folds}, Train folds: {train_folds}")
+        print(f"Fold save dir {save_fold_dir}")
+
+        train_dataset = StackingDataset(folds_data, train_folds,
+                                        get_transforms(True),
+                                        DATASET_SIZE)
+        val_dataset = StackingDataset(folds_data, val_folds,
+                                      get_transforms(False))
+
+        train_loader = DataLoader(train_dataset,
+                                  batch_size=random_params['batch_size'],
+                                  shuffle=True, drop_last=True,
+                                  num_workers=NUM_WORKERS)
+        val_loader = DataLoader(val_dataset,
+                                batch_size=random_params['batch_size'] * 2,
+                                shuffle=False, num_workers=NUM_WORKERS)
+
+        model = StackingModel(params)
+
+        callbacks = [
+            MonitorCheckpoint(save_fold_dir, monitor='val_lwlrap', max_saves=1),
+            ReduceLROnPlateau(monitor='val_lwlrap',
+                              patience=random_params['patience'],
+                              factor=random_params['factor'],
+                              min_lr=1e-8),
+            EarlyStopping(monitor='val_lwlrap', patience=50),
+            LoggingToFile(save_fold_dir / 'log.txt'),
+        ]
+
+        model.fit(train_loader,
+                  val_loader=val_loader,
+                  max_epochs=300,
+                  callbacks=callbacks,
+                  metrics=['multi_accuracy', 'lwlrap'])
+
+
+if __name__ == "__main__":
+    with open(SAVE_DIR / 'source.py', 'w') as outfile:
+        outfile.write(open(__file__).read())
+
+    if CORRECTIONS:
+        with open(config.corrections_json_path) as file:
+            corrections = json.load(file)
+        print("Corrections:", corrections)
+    else:
+        corrections = None
+
+    folds_data = get_out_of_folds_data(EXPERIMENTS, corrections)
+
+    for num in range(START_FROM, 10000):
+        np.random.seed(num)
+        random.seed(num)
+
+        save_dir = SAVE_DIR / f'{num:04}'
+        train_folds(save_dir, folds_data)
+        time.sleep(5.0)
+        torch.cuda.empty_cache()
+        time.sleep(5.0)
