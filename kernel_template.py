f724a87fc91adfc5906b72fcaa5911d5ce6f1761
ruslan <ruslan1123@gmail.com>
2019-06-01 10:58:43 +0300

    feat: Experiment auxiliary_018

--- kernel_template.py
+++ kernel_template.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import Dict
 
-EXPERIMENT_NAME = 'auxiliary_017'
+EXPERIMENT_NAME = 'auxiliary_018'
 KERNEL_MODE = "predict"  # "train" or "predict"
 
 # this is base64 encoded source code
