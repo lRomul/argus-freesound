2520849c20e3b3463ad5b28b7bf51c028b13b179
ruslan <ruslan1123@gmail.com>
2019-08-10 20:28:57 +0300

    feat: Experiment mini_asa_coreml_002

--- train_folds.py
+++ train_folds.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--experiment', required=True, type=str)
 args = parser.parse_args()
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 CROP_SIZE = 256
 DATASET_SIZE = 128 * 256
 NOISY_PROB = 0.01
